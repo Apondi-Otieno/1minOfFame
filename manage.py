cc96578a1e54412251ed1ce3569f6c939782f1e7
Apondi-Otieno <apondiotii@gmail.com>
2022-03-10 06:52:19 +0300

    manage.py file

--- manage.py
+++ manage.py
@@ -2,3 +2,13 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 from app.models import User
+
+
+
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
