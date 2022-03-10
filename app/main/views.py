8c403a59f3f7de3a022dc997bd4800cb10b9915b
Apondi-Otieno <apondiotii@gmail.com>
2022-03-10 08:21:09 +0300

    this will add user profile information

--- app/main/views.py
+++ app/main/views.py
@@ -34,4 +34,32 @@ def new_pitch():
         
     return render_template('new_pitch.html', form = form)
 
+# this is the add comments route
 
+@main.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
+@login_required
+def comment(pitch_id):
+    form = CommentForm()
+    pitch = Pitch.query.get(pitch_id)
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data 
+        pitch_id = pitch_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(comment = comment,user_id = user_id,pitch_id = pitch_id)
+        new_comment.save_c()
+        return redirect(url_for('.comment', pitch_id = pitch_id))
+    return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
+
+
+# this will add user profile information
+
+@main.route('/user/<name>')
+def profile(name):
+    user=User.query.filter_by(username=name).first()
+    user_id=current_user._get_current_object().id
+    posts=Pitch.query.filter_by(user_id=user_id).all()
+    if user is None:
+        abort(404)
+        
+    return render_template("profile/prfile.html", user=user, posts=posts)    
