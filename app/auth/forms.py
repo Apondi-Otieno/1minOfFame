66e1552c7eec40a238a6efb21113c54f6903bf95
Apondi-Otieno <apondiotii@gmail.com>
2022-03-10 08:59:57 +0300

    this will add sign up form class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,3 +8,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators=[Required()])
     remember = BooleanField('Remember Me!')
     submit = SubmitField('Login')
+    
+
+class RegForm(FlaskForm):
+    email = StringField('Your Email Address', validators=[Required(),Email()])
+    username = StringField('Enter Your Username', validators=[Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')    
