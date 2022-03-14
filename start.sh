e4412075e450c13502146b2dafffc35c2e2f9966
Apondi-Otieno <apondiotii@gmail.com>
2022-03-14 15:28:47 +0300

    bulk commit

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='mynameischebabai'
-export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:1234@localhost/pitch'
-export MAIL_USERNAME="apondioti@gmail.com"
-export MAIL_PASSWORD='34850829amk'
+# export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://access:1234@localhost/'
+# export MAIL_USERNAME="apondioti@gmail.com"
+# export MAIL_PASSWORD='34850829amk'
 
 python3 manage.py server
